cc6f413b9d91828f161079a6559587cb18290607
=Rafael <rafael@one-shift.com>
2020-08-28 17:37:35 +0100

    Class data

--- src/cheese.ts
+++ src/cheese.ts
@@ -12,6 +12,18 @@ type Settings = {
 };
 
 class Cheese {
+  stream = "video";
+  canvas = "canvas";
+  mode = "photo";
+
+  constrains = {
+    video: {
+      width: 1080,
+      height: 1080,
+      frameRate: 15,
+    }
+  };
+
   constructor(settings: Settings) {
     console.log(settings);
   }
